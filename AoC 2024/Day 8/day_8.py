bcbef14146b838d94672cdaf24b312145d1dd9ed
Esina Ranee <esina.ranee@gmail.com>
2024-12-08 11:42:23 +0000

    Merged part 1 and part 2 into a single function, added a function for checking if an antinode is within bounds. Makes it slightly slower but more readable

--- AoC 2024/Day 8/day_8.py
+++ AoC 2024/Day 8/day_8.py
@@ -38,24 +38,22 @@ for row_index in range(total_rows):
             else:
                 antenna_locations[antenna_type] = [(row_index, col_index)]
 
-def get_antinode_locations(antenna_1, antenna_2):
-    """Calculates the distance between two antennas, finds coordinates of two antinodes, returns coordinates if within bounds.
-    Returns a list of valid tuples eg [(row, col), (row, col)]"""
-    row_difference = antenna_2[0] - antenna_1[0] # y change
-    col_difference = antenna_2[1] - antenna_1[1] # x change
-    antinode_1 = (antenna_1[0] - row_difference, antenna_1[1] - col_difference) # first direction
-    antinode_2 = (antenna_2[0] + row_difference, antenna_2[1] + col_difference) # second direction
-    antinodes_within_bounds = []
-    if antinode_1[0] >= 0 and antinode_1[0] < total_rows and antinode_1[1] >= 0 and antinode_1[1] < total_cols:
-        antinodes_within_bounds.append(antinode_1)
-    if antinode_2[0] >= 0 and antinode_2[0] < total_rows and antinode_2[1] >= 0 and antinode_2[1] < total_cols:
-        antinodes_within_bounds.append(antinode_2)
-    return antinodes_within_bounds
+def coord_within_bounds(antinode_coords):
+    """Returns True or False according to if the coordinates are within the input area"""
+    return antinode_coords[0] >= 0 and antinode_coords[0] < total_rows and antinode_coords[1] >= 0 and antinode_coords[1] < total_cols
 
-def part2_get_antinode_locations(antenna_1, antenna_2):
+def get_antinode_locations(antenna_1, antenna_2, part2=False):
     """Calculates smallest repeating distance between two nodes, finds all nodes along the line, returns coordinates if within bounds."""
     row_difference = antenna_2[0] - antenna_1[0]
     col_difference = antenna_2[1] - antenna_1[1]
+
+    if part2 == False:
+        # just take the difference between the coords and return the antinodes that distance again, if within input area
+        antinode_1 = (antenna_1[0] - row_difference, antenna_1[1] - col_difference) # first direction
+        antinode_2 = (antenna_2[0] + row_difference, antenna_2[1] + col_difference) # second direction
+        return [x for x in [antinode_1, antinode_2] if coord_within_bounds(x)] # return a list of only the valid antinodes
+
+    # There could be antinodes between the antennas if they are sufficiently far apart
     greatest_common_divisor = gcd(row_difference, col_difference)
     smallest_row_difference = row_difference / greatest_common_divisor
     smallest_col_difference = col_difference / greatest_common_divisor
@@ -63,21 +61,21 @@ def part2_get_antinode_locations(antenna_1, antenna_2):
     antinodes_within_bounds = [antenna_1] # start at antenna 1, work backwards until out of range, then work forwards until out of range
 
     loop_direction_1 = True
-    next_antinode = (antenna_1[0] - smallest_row_difference, antenna_1[1] - smallest_col_difference) # set it to the starting node
+    next_antinode = (antenna_1[0] - smallest_row_difference, antenna_1[1] - smallest_col_difference) # set it to one change away from the starting node
     while loop_direction_1:
         # go from latest antinode in the given direction
         next_antinode = (antinodes_within_bounds[-1][0] - smallest_row_difference, antinodes_within_bounds[-1][1] - smallest_col_difference)
         # is it within bounds?
-        if next_antinode[0] >= 0 and next_antinode[0] < total_rows and next_antinode[1] >= 0 and next_antinode[1] < total_cols: 
+        if coord_within_bounds(next_antinode): 
             antinodes_within_bounds.append(next_antinode)
-        else:
+        else: # left the input area
             loop_direction_1 = False
 
     loop_direction_2 = True
-    next_antinode = (antenna_1[0] + smallest_row_difference, antenna_1[1] + smallest_col_difference) # reset it to the starting node
+    next_antinode = (antenna_1[0] + smallest_row_difference, antenna_1[1] + smallest_col_difference) # reset it to one in the other direction from the starting node
     while loop_direction_2:
         next_antinode = (antinodes_within_bounds[-1][0] + smallest_row_difference, antinodes_within_bounds[-1][1] + smallest_col_difference)
-        if next_antinode[0] >= 0 and next_antinode[0] < total_rows and next_antinode[1] >= 0 and next_antinode[1] < total_cols:
+        if coord_within_bounds(next_antinode):
             antinodes_within_bounds.append(next_antinode)
         else:
             loop_direction_2 = False
@@ -92,7 +90,7 @@ for list_of_locations in antenna_locations.values(): # format '#': [[1, 3], [2, 
     list_of_pairs = list(combinations(list_of_locations, 2))
     for pair in list_of_pairs:
         list_of_antinode_locations += get_antinode_locations(pair[0], pair[1]) # may include duplicates
-        part2_list_of_antinode_locations += part2_get_antinode_locations(pair[0], pair[1]) # may include duplicates
+        part2_list_of_antinode_locations += get_antinode_locations(pair[0], pair[1], part2=True) # may include duplicates
 # remove duplicate values
 unique = list(dict.fromkeys(list_of_antinode_locations))
 part2_unique = list(dict.fromkeys(part2_list_of_antinode_locations))
@@ -107,4 +105,4 @@ timer_script_end=perf_counter()
 
 print(f"""Execution times (sec)
 Parse: {timer_parse_end-timer_parse_start:3.3f}
-Total: {timer_script_end-timer_script_start:3.3f}""")
+Total: {timer_script_end-timer_script_start:3.5f}""")
